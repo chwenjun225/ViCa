1667add108ab13a58f833c5259cedea4d05a5205
chwenjun225 <trantuan22052k@gmail.com>
2023-09-14 15:31:39 +0800

    Update files

--- test3.py
+++ test3.py
@@ -1,1 +1,12 @@
-print("test3, test git")
+"""
+Input image 
+Resize image to 416x416
+YOLOv8 detect barcode, QR-code on image-resized
+Crop resized image follow with the YOLOv8 coordinates 
+REAL-ESRGAN super-resolution on croped-image 
+Pyzbar decode barcode on croped-image 
+"""
+"""
+Pseudo code:
+
+"""
