c6bed4561ce9a1bd3bf1511616e27fcb18b2937b
chwenjun225 <trantuan22052k@gmail.com>
2023-09-20 17:12:22 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	test4.py

--- test4.py
+++ test4.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import time
-=======
-import time 
->>>>>>> origin/master
 import random
 from barcode import EAN13
 from barcode.writer import ImageWriter
@@ -11,7 +6,6 @@ from barcode.writer import ImageWriter
 def generate_barcode():
     count_index = 0
 
-<<<<<<< HEAD
     while count_index < 1000:
         number_in_barcode = str(random.random()).replace(".", "")
         my_code = EAN13(number_in_barcode, writer=ImageWriter())
@@ -19,28 +13,6 @@ def generate_barcode():
         count_index += 1
 
 
-if __name__ == "__main__":
-
-    RUN_ = 1
-    if 1 == RUN_:
-        generate_barcode()
-=======
-    while count_index < 1000: 
-        number_in_barcode = str(random.random()).replace(".", "")
-        my_code = EAN13(number_in_barcode, writer=ImageWriter())
-        my_code.save(f"OpenSR_barcode_images/{count_index}")
-        count_index += 1
-
-
-if __name__=="__main__":
-
-    RUN_ = 1
-    if 1 == RUN_:
-
-        start_time = time.time()
-
-        generate_barcode()
-        
-        print("The time of execution of above program is :", 
-        (end-start) * 10**3, "ms")
->>>>>>> origin/master
+RUN_ = 1
+if 1 == RUN_:
+    generate_barcode()
