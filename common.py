6182c313bda7be660013aeeb7962e21efa878064
chwenjun225 <trantuan22052k@gmail.com>
2024-10-22 14:59:17 +0800

    Add downloader, common, comm_runtime

--- common.py
+++ common.py
@@ -2,3 +2,120 @@ import argparse
 import os 
 
 import tensorrt as trt 
+from common_runtime import * 
+
+try: 
+    # Sometimes python does not understand FileNotFoundError 
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
+def GiB(val):
+    return val * 1 << 30 
+
+def add_help(description):
+    parser = argparse.ArgumentParser(
+        description=description, 
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    args, _ = parser.parse_known_args()
+
+def find_sample_data(
+        description="Runs a TensorRT Python sample", subfolder="", 
+        find_files=[], 
+        err_msg="", 
+):
+    """
+    Parses sample arguments.
+
+    Args:
+        description (str): Description of the sample.
+        subfolder (str): The subfolder containing data relevant to this sample.
+        find_files (List[str]): List of filenames to find. Each filename will be replace with an absolute path. 
+
+    Returns:
+        str: Path of data directory. 
+    """
+    # Standard command-line arguments for all samples 
+    kDEFAULT_DATA_ROOT = os.path.join(os.sep, "usr", "src", "tensorrt", "data")
+    parser = argparse.ArgumentParser(
+        description=description, 
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-d", 
+        "--datadir", 
+        help="Location of the TensorRT sample data directory, and any additional data directories.",
+        action="append", 
+        default=[kDEFAULT_DATA_ROOT],
+    )
+    args, _ = parser.parse_known_args()
+
+    def get_data_path(data_dir):
+        # If the subfolder exists, append it to the path, otherwise use the provided path as-is 
+        data_path = os.path.join(data_dir, subfolder)
+        if not os.path.exists(data_path):
+            if data_dir != kDEFAULT_DATA_ROOT:
+                print(
+                    "WARNING: "
+                    + data_path 
+                    + " does not exist. Trying "
+                    + data_dir 
+                    +" instead."
+                )
+            data_path = data_dir 
+        # Make sure data directory exists 
+        if not (os.path.exists(data_path)) and data_dir != kDEFAULT_DATA_ROOT:
+            print(
+                "WARNING: {:} does not exist. Please provide the correct data path with the -d option.".format(data_path)
+            )
+        return data_path 
+    data_paths = [get_data_path(data_dir) for data_dir in args.datadir]
+    return data_paths, locate_files(data_paths, find_files, err_msg)
+
+def locate_files(data_paths, filenames, err_msg=""):
+    """
+    Locates the specified files in the specified directories. 
+    If a file exists in multiple data directories, the first directory is used. 
+
+    Args:
+        data_paths (List[str]): The data directories. 
+        filename (List[str]): The names of the files to find. 
+
+    Returns:
+        List[str]: The absolute paths of the files. 
+    
+    Raises:
+        FileNotFoundError: If a file could not be located. 
+    """
+    found_files = [None] * len(filenames)
+    for data_path in data_paths:
+        for index, (found, filename) in enumerate(zip(found_files, filenames)):
+            if not found:
+                file_path = os.path.abspath(os.path.join(data_path, filename))
+                if os.path.exists(file_path):
+                    found_files[index] = file_path 
+    
+    # Check that all files were found 
+    for f, filename in zip(found_files, filenames):
+        if not f or not os.path.exists(f):
+            raise FileNotFoundError(
+                "Could not find {:}. Searched in data paths: {:}\n{:}".format(filename, data_paths, err_msg)
+            )
+    return found_files
+
+# Sets up the builder to use the timing cache file, and creates it if it does not exist 
+def setup_timing_cache(config: trt.IBuilderConfig, timing_cache_path: os.PathLike):
+    buffer = b""
+    if os.path.exists(timing_cache_path):
+        with open(timing_cache_path, mode="rb") as timing_cache_file:
+            buffer = timing_cache_file.read()
+    timing_cache: trt.ITimingCache = config.create_timing_cache(buffer)
+    config.set_timing_cache(timing_cache, True)
+
+# Save the config's timing cache to file 
+def save_timing_cache(config: trt.IBuilderConfig, timing_cache_path: os.PathLike):
+    timing_cache: trt.ITimingCache = config.get_timing_cache()
+    with open(timing_cache_path, "wb") as timing_cache_file:
+        timing_cache_file.write(memoryview(timing_cache.serialize()))
+        
--- common_runtime.py
+++ common_runtime.py
@@ -42,23 +42,105 @@ class HostDeviceMem:
     def host(self, data: Union[np.ndarray, bytes]):
         if isinstance(data, np.ndarray):
             if data.size > self.host.size:
-                pass
+                raise ValueError(
+                    f"Tried to fit an array to size {data.size} into host memory of size {self.host.size}"
+                )
+            np.copyto(self.host[:data.size], data.flat, casting='safe')
+        else:
+            assert self.host.dtype == np.uint8 
+            self.host[:self.nbytes] = np.frombuffer(data, dtype=np.unint8)
+    
+    @property 
+    def device(self) -> int:
+        return self._device 
+    
+    @property
+    def nbytes(self) -> int:
+        return self._nbytes 
 
-def allocate_buffers():
-    pass 
+    def __str__(self):
+        return f"Host:\n{self.host}\nDevice:\n{self.device}\nSize:\n{self.nbytes}\n"
+    
+    def __repr__(self):
+        return self.__str__()
 
-def free_buffers():
-    pass
+    def free(self):
+        cuda_call(cudart.cudaFree(self.device))
+        cuda_call(cudart.cudaFreeHost(self.host.ctypes.data))
 
-def memcpy_host_to_device():
-    pass
+# Allocates all buffers required for an engine, i.e. host/device inputs/outputs. If 
+# engine uses dynamic shapes, specify a profile to find the maximum input & output size. 
+def allocate_buffers(engine:trt.ICudaEngine, profile_idx:Optional[int]=None):
+    inputs = []
+    outputs = []
+    bindings = []
+    stream = cuda_call(cudart.cudaStreamCreate())
+    tensor_names = [engine.get_tensor_name(i) for i in range(engine.num_io_tensors)]
+    for binding in tensor_names:
+        # get_tensor_profile_shape returns (min_shape, optimal_shape, max_shape)
+        # Pick out the max shape to allocate enough memory for the binding.
+        shape = engine.get_tensor_shape(binding) if profile_idx is None else engine.get_tensor_profile_shape(binding, profile_idx)[-1]
+        shape_valid = np.all([s >= 0] for s in shape)
+        if not shape_valid and profile_idx is None:
+            raise ValueError(f"Binding {binding} has dynamic shape, " + "but no profile was specified")
+        size = trt.volume(shape)
+        trt_type = engine.get_tensor_dtype(binding)
 
-def memcpy_device_to_host():
-    pass
+        # Allocate host and device buffers 
+        try:
+            dtype = np.dtype(trt.nptype(trt_type))
+            bindingMemory = HostDeviceMem(size, dtype)
+        except TypeError: # no numpy support: create a byte array instead (BF16, FP8, INT4)
+            size = int(size * trt_type.itemsize)
+            bindingMemory = HostDeviceMem(size)
+        
+        # Append the device buffer to device bindings
+        bindings.append(int(bindingMemory.device))
 
-def _do_inference_base():
-    pass
+        # Append to the appropriate list
+        if engine.get_tensor_mode(binding) == trt.TensorIOMode.INPUT:
+            inputs.append(bindingMemory)
+        else:
+            outputs.append(bindingMemory)
+    return inputs, outputs, bindings, stream
 
+# Frees the resources allocated in allocate_buffers 
+def free_buffers(inputs:List[HostDeviceMem], outputs:List[HostDeviceMem], stream:cudart.cudaStream_t):
+    for mem in inputs + outputs:
+        mem.free()
+    cuda_call(cudart.cudaStreamDestroy(stream)) 
 
-def do_inference():
-    pass
+# Wrapper for cudaMemcpy which infers copy size and does error checking 
+def memcpy_host_to_device(device_ptr:int, host_arr:np.ndarray):
+    nbytes = host_arr.size * host_arr.itemsize 
+    cuda_call(cudart.cudaMemcpy(device_ptr, host_arr, nbytes, cudart.cudaMemcpyKind.cudaMemcpyHostToDevice))
+
+# Wrapper for cudaMemcpy which infers copy size and does error checking 
+def memcpy_device_to_host(host_arr:np.ndarray, device_ptr:int):
+    nbytes = host_arr.size * host_arr.itemsize
+    cuda_call(cudart.cudaMemcpy(host_arr, device_ptr, nbytes, cudart.cudaMemcpyKind.cudaMemcpyDeviceHost))
+
+def _do_inference_base(inputs, outputs, stream, execute_async_func):
+    # Transfer input data to the GPU 
+    kind = cudart.cudaMemcpyKind.cudaMemcpyHostToDevice 
+    [cuda_call(cudart.cudaMemcpyAsync(inp.device, inp.host, inp.nbytes, kind, stream)) for inp in inputs]
+    # Run inference 
+    execute_async_func()
+    # Transfer predictions back from the GPU 
+    kind = cudart.cudaMemcpyKind.cudaMemcpyDeviceToHost
+    [cuda_call(cudart.cudaMemcpyAsync(out.host, out.device, out.nbytes, kind, stream)) for out in outputs]
+    # Synchronize the stream 
+    cuda_call(cudart.cudaStreamSynchronize(stream))
+    # Return only the host outputs
+    return [out.host for out in outputs]
+
+# This function is generalized for multiple inputs/outputs. inputs
+# and outputs are expected to be lists of HostDeviceMem objects
+def do_inference(context, engine, bindings, inputs, outputs, stream):
+    def execute_async_func():
+        context.execute_async_v3(stream_handle=stream)
+    # Setup context tensor address
+    num_io = engine.num_io_tensors
+    for i in range(num_io):
+        context.set_tensor_address(engine.get_tensor_name(i), bindings[i])
+    return _do_inference_base(inputs, outputs, stream, execute_async_func)
